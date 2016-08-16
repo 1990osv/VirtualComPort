55c3ca430cb471a0673a9e11c308e3a5890fb692
Seragdin <1990osv@rambler.ru>
2016-08-16 19:24:33 +0300

    CRC работает!!!

--- VirtualComPort/Protocol/protocol.c
+++ VirtualComPort/Protocol/protocol.c
@@ -9,7 +9,7 @@
 #define INTERFACE_STATE_CNT     4
 
 uint8_t lastCiclCount;     //номер предыдущего сообщения
-uint8_t inMsgCrc;     //контрольная сумма принятого сообщения
+
 
 struct StructInMsg{
         unsigned char ciclCount;
@@ -48,49 +48,60 @@ union OutMsg {
 } out;
 
 
-unsigned char crcCompute(unsigned char *data, unsigned char len);
+/*Объявление статических функций*/
+
+static void setDataFaultFlag(void);
+static void resetDataFaultFlag(void);
+static unsigned char crcCompute(unsigned char *data, unsigned char len);
+static unsigned char crcOutCompute(unsigned char *data, unsigned char len);
 
 void setDataFaultFlag(void)
 {
         out.msg.stateL |= (1<<(INTERFACE_STATE));
+        HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,GPIO_PIN_SET);        
 }
 
 void resetDataFaultFlag(void)
 {
         out.msg.stateL &= (~(1<<(INTERFACE_STATE)));
+        HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,GPIO_PIN_RESET);
 }
 
 void transfer(void)
 {
         uint32_t Len = IN_MSG_SIZE;
-
+        uint8_t inMsgCrc;     //контрольная сумма принятого сообщения
         if (CDC_Receive_FS(in.buf, &Len) == USBD_OK)
         {
-                inMsgCrc = crcCompute(in.buf, IN_MSG_SIZE - 1) - 1;
-                if ((lastCiclCount != in.msg.ciclCount) && ( inMsgCrc == in.msg.crc))
+                inMsgCrc = crcCompute(in.buf, IN_MSG_SIZE - 1);
+                if(lastCiclCount != in.msg.ciclCount)
                 {
                         lastCiclCount = in.msg.ciclCount;
                         out.msg.ciclCount = in.msg.ciclCount;
-                        out.msg.azimutL = in.msg.crc;
-                        out.msg.azimutH = inMsgCrc;
-                        out.msg.crc = crcCompute(out.buf,OUT_MSG_SIZE - 1);
-                        CDC_Transmit_FS(out.buf, OUT_MSG_SIZE);
-                        HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_12);                        
 
-//                        HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,GPIO_PIN_SET);
+                        if ( inMsgCrc == in.msg.crc )
+                        {
+                                out.msg.azimutL = 0;
+                                out.msg.azimutH = 0;
+                                resetDataFaultFlag();                     
+                        }
+                        else
+                        {
+                                out.msg.azimutL = in.msg.crc;
+                                out.msg.azimutH = inMsgCrc;
+                                setDataFaultFlag();
+                        }
+                        out.msg.crc = crcOutCompute(out.buf,OUT_MSG_SIZE - 1);
+                        CDC_Transmit_FS(out.buf, OUT_MSG_SIZE);        
                 }
-//                else
-//                {
-//                        HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,GPIO_PIN_RESET);
-//                }
-                        
         }
 }
 
 unsigned char crcCompute(unsigned char *data, unsigned char len)
 {
-        unsigned char i, crcComp;
-        crcComp = data[0];
+        volatile unsigned char i, crcComp;
+       
+        crcComp = data[0] - 1;
         for (i = 1; i < len; i++)
         {
                 crcComp ^= data[i];
@@ -98,5 +109,15 @@ unsigned char crcCompute(unsigned char *data, unsigned char len)
         return crcComp;
 }
 
-
+unsigned char crcOutCompute(unsigned char *data, unsigned char len)
+{
+        volatile unsigned char i, crcComp;
+       
+        crcComp = data[0];
+        for (i = 1; i < len; i++)
+        {
+                crcComp ^= data[i];
+        }
+        return crcComp;
+}
 
